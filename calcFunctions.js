c4a7aba5c62afe93d8b78fe66cb57001c2529e96
HDBarton <heatherdbarton@gmail.com>
2022-08-09 17:51:57 -0700

    Add requirements as notes

--- calcFunctions.js
+++ calcFunctions.js
@@ -1,1 +1,26 @@
-//Create a calculator with buttons that can add, subtract, multiply and divide a pair of numbers at a time
+// Create a calculator with buttons that can add, subtract, multiply and divide a pair of numbers at a time
+// Create a function for each math action (round answers with long decimal values)
+
+
+// Create function operate() that takes an operator and 2 numbers and then calls one of the action functions
+// Error message for dividing by zero
+// Need a response for pressing = without enough values
+// "Clear" should wipe out existing data
+
+// Take in a store the pushed botton values
+
+// Calculate when the equals button is pushed
+
+// Populate the display when each button is clicked and store the display value
+
+// Update the display with the calcuation's solution 
+
+
+
+
+
+
+
+// EXTRA CREDIT: Add a . for decimal value math (but only up to tenths place).
+// also add a backspace button
+// add keyboard support
