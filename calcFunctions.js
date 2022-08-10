3729444419b46473e501ac336846e2bcc535f98b
HDBarton <heatherdbarton@gmail.com>
2022-08-10 16:46:23 -0700

    Add clear functionality

--- calcFunctions.js
+++ calcFunctions.js
@@ -1,7 +1,8 @@
 // Create a calculator with buttons that can add, subtract, multiply and divide a pair of numbers at a time
 // Create an array for numbers, and for operators to populate from onclick eventlistener
-const numInputs = [];
-const opInputs = [];
+let numInputs = [];
+let opInputs = [];
+let displayBox = document.getElementById("display");
 // Create a function for each math action (round answers with long decimal values)
 function add(num1, num2){
     let number1 = parseInt(num1);
@@ -39,37 +40,43 @@ let numButtons = document.querySelectorAll(".sqBtn");
 
 numButtons.forEach(item => {item.addEventListener('click', function(){
     numInputs.push(item.value);
-    document.getElementById("display").innerHTML = item.value;
+    displayBox.innerHTML = item.value;
 })});
 let operators = document.querySelectorAll(".actionBtn");
 
 operators.forEach(item => {item.addEventListener('click', function(){
     opInputs.push(item.value);
-    document.getElementById("display").innerHTML = item.value;
+    displayBox.innerHTML = item.value;
 })});
 let equalBtn = document.getElementById("equals");
 
-equals.addEventListener('click', function(){
+equalBtn.addEventListener('click', function(){
     let num2 = numInputs.pop();
     let num1 = numInputs.pop();
     let operator = opInputs.pop();
     if (operator == "+"){
-        document.getElementById("display").innerHTML = add(num1, num2);
+        displayBox.innerHTML = add(num1, num2);
     }else if (operator == "-"){
-        document.getElementById("display").innerHTML = subtract(num1, num2);
+        displayBox.innerHTML = subtract(num1, num2);
     }else if (operator == "x"){
-        document.getElementById("display").innerHTML = multiply(num1, num2);
+        displayBox.innerHTML = multiply(num1, num2);
     }else {
-        document.getElementById("display").innerHTML = divide(num1, num2);
+        displayBox.innerHTML = divide(num1, num2);
     }
 });
 
 // Create function operate() that takes an operator and 2 numbers and then calls one of the action functions
 // Calculate when the equals button is pushed
 // Need a response for pressing = without enough values
+
 // "Clear" should wipe out existing data
+let clearBtn = document.getElementById("clear");
 
-
+clearBtn.addEventListener('click', function(){
+    numInputs = [];
+    opInputs = [];
+    displayBox.innerHTML = "";
+});
 // Update the display with the calcuation's solution 
 
 
